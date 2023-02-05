b8d37996bbb0ed756982582d59c930329e3b3c35
Marcos Falcon <marcosfalcon2003@gmail.com>
2023-02-04 19:08:27 -0600

    Listas
    Crear lista, insertar elementos a la lista al principio

---
+++ listas/Listas.java
@@ -0,0 +1,15 @@
+package com.mycompany.listas;
+
+public class Listas {
+
+    public static void main(String[] args) {
+        
+        Lista lista = new Lista();
+       
+        lista.addNodoI(4);
+        lista.addNodoI(5);
+        
+        lista.mostrarLista();
+    }
+}
+
