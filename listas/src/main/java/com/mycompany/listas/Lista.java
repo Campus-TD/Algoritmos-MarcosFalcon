10efc2878727fa0fe4dc418e62035d3f9794199c
MarcosFalcon343 <marcosfalcon2003@gmail.com>
2023-03-21 23:58:48 -0600

    update

--- listas/src/main/java/com/mycompany/listas/Lista.java
+++ listas/src/main/java/com/mycompany/listas/Lista.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.listas;
 
 public class Lista {
@@ -20,7 +19,7 @@ public class Lista {
         return ((double)this.promedio / (double) this.i);
     }
     
-    public void addNodoI(int valor){
+    public void II(int valor){
         Nodo nuevo = new Nodo();
         nuevo.valor = valor;
         if(Raiz != null){
@@ -34,26 +33,38 @@ public class Lista {
         this.moda(valor);
     }
     
-    public void addNodoF(int valor){
-        Nodo nuevo = new Nodo();
-        nuevo.valor = valor;
-        if(Raiz == null){
-            Raiz = nuevo;
-        }else{
-            Nodo aux = this.Raiz;
-            while(aux.siguiente != null){
-                aux = aux.siguiente;
-            }
-            aux.siguiente = nuevo;
+    public void mostrarTodo(){
+        Nodo aux = new Nodo();
+        aux = Raiz;
+        while(aux != null){
+            System.out.print(aux.valor + " ");
+            aux = aux.siguiente;
         }
-        
-        this.promedio(valor);
-        this.moda(valor);
+        System.out.println("");
     }
     
-    private void promedio(int valor){
-        this.i++;
-        promedio += valor;
+    public void mostrarMedio(){
+        int i = 0;
+        Nodo aux = this.Raiz;
+        while(aux != null){
+            i++;
+            aux = aux.siguiente;
+        }
+        int m;
+        aux = this.Raiz;
+        if(i % 2 == 0){
+            m = i / 2;
+            for(int j = 0; j < m-1; j++){
+                aux = aux.siguiente;
+            }
+            System.out.println(aux.valor + " " + aux.siguiente.valor);
+        }else{
+            m = Math.round(i/2);
+            for(int j = 0; j < m; j++){
+                aux = aux.siguiente;
+            }
+            System.out.println(aux.valor);
+        }
     }
     
     private void moda(int valor){
@@ -62,16 +73,6 @@ public class Lista {
             this.mayor = this.A[valor];
             this.moda = valor;
         }
-    }
-    
-    public void mostrarLista(){
-        Nodo aux = new Nodo();
-        aux = Raiz;
-        while(aux != null){
-            System.out.print(aux.valor + " ");
-            aux = aux.siguiente;
-        }
-        System.out.println("");
     }
     
     public void mostrarPar(){
@@ -97,6 +98,159 @@ public class Lista {
         }
         System.out.println("");
     }
+    
+    public void IF(int valor){
+        Nodo nuevo = new Nodo();
+        nuevo.valor = valor;
+        if(Raiz == null){
+            Raiz = nuevo;
+        }else{
+            Nodo aux = this.Raiz;
+            while(aux.siguiente != null){
+                aux = aux.siguiente;
+            }
+            aux.siguiente = nuevo;
+        }
+        
+        this.promedio(valor);
+        this.moda(valor);
+    }
+    
+    
+    public void partirVoltear(){
+        int i = 0,m;
+        Nodo aux = this.Raiz;
+        while(aux != null){
+            i++;
+            aux = aux.siguiente;
+        }
+        m = i / 2;
+        
+        aux = this.Raiz;
+        Lista RA = new Lista();
+        int j = 0;
+        aux = this.Raiz;
+        while(aux.siguiente != null){
+            if(j < m)RA.IF(aux.valor);
+            j++;
+            aux = aux.siguiente;
+        }
+        aux.siguiente = RA.Raiz;
+        aux = this.Raiz;
+        for(j = 0; j < m; j++){
+            aux = aux.siguiente;
+        }
+        this.Raiz = aux;
+    }
+    
+    //Corregir
+    public void IE(int valor, int pos){
+        Nodo nuevo = new Nodo();
+        nuevo.valor = valor;
+        Nodo aux = this.Raiz;
+        
+        if(aux == null){
+            this.Raiz = nuevo;
+        }else{
+            int i = 1;
+            pos--;
+            while(i < pos && aux.siguiente != null){
+                aux = aux.siguiente;
+                i++;
+            }
+            nuevo.siguiente = aux.siguiente;
+            aux.siguiente = nuevo;
+        }
+        
+    }
+    
+    public void reversaIterativa(){
+        
+    }
+    
+    public void eliminar(int valor){
+        Nodo aux = this.Raiz;
+        Nodo aux2;
+        if(aux.valor == valor){
+            this.Raiz = aux.siguiente;
+        }else{
+            aux2 = null;
+            while(aux != null && aux.valor != valor){
+                aux2 = aux;
+                aux = aux.siguiente;
+            }
+            
+            if(aux != null){
+                aux2.siguiente = aux.siguiente;
+            }
+        }
+    }
+    
+    public void mostrarUSUN(){
+        Nodo aux = this.Raiz;
+        boolean ck = true;
+        while(aux != null){
+            if(ck){
+                System.out.println(aux.valor + " ");
+            }
+            ck = !ck;
+            aux = aux.siguiente;
+        }
+    }
+    
+    public void mostrarUSASC(){
+        Nodo aux = this.Raiz;
+        boolean ck = true;
+        int i = 0, j = 2;
+        while(aux != null){
+            if(ck){
+                System.out.println(aux.valor + " ");
+                ck = false;
+            }
+            if(i == j){
+                ck = true;
+                j++;
+                i = 0;
+            }else{
+                i++; 
+            }
+            aux = aux.siguiente;
+        }
+    }
+    
+    public void EliminarUSUN(){
+        Nodo aux = this.Raiz;
+        Nodo aux2 = null;
+        boolean ck = true;
+        while(aux != null){
+            if(ck){
+                aux2.siguiente = aux.siguiente;
+            }
+            aux2 = aux;
+            aux = aux.siguiente;
+        }
+    }
+    
+    public void ElimiarUSASC(){
+        
+    }
+    
+    public void afueraDentro(){
+        
+    }
+    
+    public void adentroFuera(){
+        
+    }
+    
+    public void Solo(){
+        
+    }
+    
+    private void promedio(int valor){
+        this.i++;
+        promedio += valor;
+    }
+    
+    
 }
-
-
--- listas/src/main/java/com/mycompany/listas/Listas.java
+++ listas/src/main/java/com/mycompany/listas/Listas.java
@@ -3,14 +3,22 @@ package com.mycompany.listas;
 public class Listas {
 
     static String[] menu = {
-        "1.-Insertar nodo al inicio",
+        "\n1.-Insertar nodo al inicio",
         "2.-Mostrar lista",
         "3.-Imprimir promedio",
         "4.-Insertar ultimo",
         "5.-Moda imprimir",
         "6.-Imprimir par",
         "7.-Imprimir impar",
-        "8.-Finalizar"
+        "8.-Imprimir medio",
+        "9.-Partir y voltear",
+        "10.-Insertar en",
+        "11.-Reversa (iterativa)(No terminado)",
+        "12.-Reversa (recursiva)(No terminado)",
+        "13.-Eliminar numero",
+        "14.-Mostrar uno si uno no",
+        "15.-Mostrar uno si dos no, uno si tres no...",
+        "16.-Eliminar Uno si, Uno no.."
             
     };
     static Lista lista = new Lista();
@@ -23,25 +31,41 @@ public class Listas {
             
             switch(op){
                 case "1" -> insertarInicio();
-                case "2" -> lista.mostrarLista();
+                case "2" -> lista.mostrarTodo();
                 case "3" -> System.out.println(lista.getPromedio());
                 case "4" -> insertarFinal();
                 case "5" -> System.out.println(lista.getModa());
                 case "6" -> lista.mostrarPar();
                 case "7" -> lista.mostrarImpar();
-                case "8" -> System.out.println("Finalizado");
+                case "8" -> lista.mostrarMedio();
+                case "9" -> lista.partirVoltear();
+                case "10" -> insertarEn();
+                case "13" -> eliminar();
+                case "14" -> lista.mostrarUSUN();
+                case "15" -> lista.mostrarUSASC();
+                case "16" -> lista.EliminarUSUN();
             }
-        }while(!op.equals("8"));
+        }while(!op.equals("11"));
     }
     
     public static void insertarInicio(){
         int valor = Libreria.validarInt("Ingrese el valor que tendra el nodo");
-        lista.addNodoI(valor);
+        lista.II(valor);
     }
     
     public static void insertarFinal(){
         int valor = Libreria.validarInt("Ingrese el valor que tendra el nodo");
-        lista.addNodoF(valor);
+        lista.IF(valor);
+    }
+    
+    public static void insertarEn(){
+        int valor = Libreria.validarInt("Ingrese el valor que tendra el nodo");
+        int pos = Libreria.validarInt("Ingrese la posicion a insertar el nodo");
+        lista.IE(valor, pos);
+    }
+    
+    public static void eliminar(){
+        int valor = Libreria.validarInt("Ingrese el valor a eliminar");
+        lista.eliminar(valor);
     }
 }
-
